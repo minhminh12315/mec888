362eb30a07170c4742bbce4cbf5bbb332284caea
Quang Minh <157796411+minhminh12315@users.noreply.github.com>
2025-04-07 17:39:47 +0700

    add entity and dao Appointment and MedicalRecord

---
+++ src/main/java/com/home/mec888/dao/AppointmentDao.java
@@ -0,0 +1,74 @@
+package com.home.mec888.dao;
+
+import com.home.mec888.entity.Appointment;
+import com.home.mec888.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class AppointmentDao {
+
+    public void saveAppointment(Appointment appointment) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(appointment);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void updateAppointment(Appointment appointment) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(appointment);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public Appointment getAppointmentById(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Appointment.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Appointment> getAllAppointments() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Appointment order by updatedAt desc", Appointment.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void deleteAppointment(Long id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Appointment appointment = session.get(Appointment.class, id);
+            if (appointment != null) {
+                session.delete(appointment);
+                transaction.commit();
+            }
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+}
