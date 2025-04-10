db9ce74bdea38a4705c6bf915286e70b88d7e89f
Minggg <ddqminh205@gmail.com>
2025-04-10 19:27:13 +0700

    create file

---
+++ src/main/java/com/home/mec888/controller/staff/appointment/AppointmentAddController.java
@@ -0,0 +1,25 @@
+package com.home.mec888.controller.staff.appointment;
+
+import javafx.event.ActionEvent;
+
+public class AppointmentAddController {
+
+    public void handleClear(ActionEvent event) {
+// Xóa lựa chọn của các ComboBox
+//        userComboBox.getSelectionModel().clearSelection();
+//        departmentComboBox.getSelectionModel().clearSelection();
+//
+//        // Xóa nội dung của các TextField
+//        specializationField.clear();
+//        licenseField.clear();
+//        firstNameField.clear();
+//        lastNameField.clear();
+//
+//        resetErrorLabels();
+    }
+
+//    public void handleBack(ActionEvent event) {
+//        returnToDoctorManagement(event);
+//    }
+
+}
